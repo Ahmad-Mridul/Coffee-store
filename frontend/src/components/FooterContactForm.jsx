d826633c121fbe29d4f08e3554a10131c2d1938c
LA Mridul <mridulahmed262@gmail.com>
2025-06-22 14:58:07 +0600

    send a message button updated

--- frontend/src/components/FooterContactForm.jsx
+++ frontend/src/components/FooterContactForm.jsx
@@ -2,9 +2,30 @@ const FooterContactForm = () => {
     return (
         <div>
             <form>
-                <input className="border mb-3 rounded text-black" type="text" name="name" placeholder="Name" /><br />
-                <input className="border mb-3 rounded text-black" type="email" name="email" placeholder="Email" /><br />
-                <textarea className="border mb-3 rounded text-black" name="message" placeholder="Message"></textarea>
+                <input
+                    className="border mb-3 rounded text-black"
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                />
+                <br />
+                <input
+                    className="border mb-3 rounded text-black"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                />
+                <br />
+                <textarea
+                    cols="23"
+                    className="border mb-3 rounded text-black"
+                    name="message"
+                    placeholder="Message"
+                ></textarea>
+                <br />
+                <button class="border border-[#331A15] text-[#331A15] hover:bg-[#331A15] hover:text-white font-medium py-2 px-4 rounded">
+                    Outline Button
+                </button>
             </form>
         </div>
     );
