dd4ebbcd96e671323023cbe87602b7c6d4567026
LA Mridul <mridulahmed262@gmail.com>
2025-06-27 01:09:26 +0600

    fixed all issues from main branch

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
-require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const { log } = require("console");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,8 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 const uri =
-	`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rhoqvhp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; // Replace with your URI
+	"mongodb+srv://birm2476:birm2476@cluster0.ryk4cdh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
 	serverApi: {
 		version: ServerApiVersion.v1,
@@ -21,9 +22,9 @@ const client = new MongoClient(uri, {
 	},
 });
 
-
 async function run() {
 	try {
+		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
 		const database = client.db("coffeeDB");
 		const coffeesCollection = database.collection("coffees");
@@ -73,14 +74,16 @@ async function run() {
 			const result = await coffeesCollection.deleteOne(query);
 			res.send(result);
 		})
-
+		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
-		console.log("Connected to MongoDB!");
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
 	} finally {
-		// await client.close();  // Keep connection open for server to work
+		// Ensures that the client will close when you finish/error
+		// await client.close();
 	}
 }
-
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
