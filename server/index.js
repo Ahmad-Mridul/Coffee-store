e84f302731c7ce91e828c780032376d6cfaea6da
Ahmad-Mridul <mridulahmed262@gmail.com>
2025-07-21 23:11:38 +0600

    deployed

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require("express");
 require('dotenv').config();
+const express = require("express");
 const { ObjectId } = require("mongodb");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
+		// await client.connect();
 
 		const mydb = client.db("coffeeDB");
 		const coffeeCollection = mydb.collection("coffees");
@@ -80,7 +80,7 @@ async function run() {
 		})
 
 		// Send a ping to confirm a successful connection
-		await client.db("admin").command({ ping: 1 });
+		// await client.db("admin").command({ ping: 1 });
 		console.log(
 			"Pinged your deployment. You successfully connected to MongoDB!"
 		);
